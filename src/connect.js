8f92b02048e3ad5cb56e7900d31bd5a80f8d714f
Iago Bruno <httpiago@gmail.com>
2018-11-28 01:21:37 -0300

    map state containers array into an object
    Transform the array into an object (the name of the classes will be the keys)

--- src/connect.js
+++ src/connect.js
@@ -3,6 +3,14 @@ import { Subscribe } from "unstated";
 
 export default _containers => Component => props => (
   <Subscribe to={_containers}>
-    {(...containers) => <Component {...props} containers={containers} />}
+    {(...containers) => {
+      // Transform the array into an object (the name of the classes will be the keys)
+      let mapContainers = containers.reduce((acc, cur) => (acc[cur.constructor.name] = cur, acc), {})
+      
+      return <Component {...props} containers={mapContainers} />
+    }}
   </Subscribe>
 );
+
+// [].reduce polyfill
+if (!Array.prototype.reduce) Array.prototype.reduce=function(d){if(null==this)throw new TypeError("Array.prototype.reduce called is null (null) or undefined (undefined)");if("function"!==typeof d)throw new TypeError(d+" is not a function");var b=Object(this),e=b.length>>>0,a=0;if(2==arguments.length)var c=arguments[1];else{for(;a<e&&!(a in b);)a++;if(a>=e)throw new TypeError("Reduce has an empty array without an initial value");c=b[a++]}for(;a<e;a++)a in b&&(c=d(c,b[a],a,b));return c};
