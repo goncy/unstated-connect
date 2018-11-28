e2075d29fb68d7464856862cfa28e0003d603d2e
Iago Bruno <httpiago@gmail.com>
2018-11-28 01:15:02 -0300

    new dist bundles

--- dist/unstated-connect.esm.js
+++ dist/unstated-connect.esm.js
@@ -14,11 +14,29 @@ var connect = (function (_containers) {
             containers[_key] = arguments[_key];
           }
 
-          return React.createElement(Component, _extends({}, props, { containers: containers }));
+          // Transform the array into an object (the name of the classes will be the keys)
+          var mapContainers = containers.reduce(function (acc, cur) {
+            return acc[cur.constructor.name] = cur, acc;
+          }, {});
+
+          return React.createElement(Component, _extends({}, props, { containers: mapContainers }));
         }
       );
     };
   };
 });
+
+// [].reduce polyfill
+if (!Array.prototype.reduce) Array.prototype.reduce = function (d) {
+  if (null == this) throw new TypeError("Array.prototype.reduce called is null (null) or undefined (undefined)");if ("function" !== typeof d) throw new TypeError(d + " is not a function");var b = Object(this),
+      e = b.length >>> 0,
+      a = 0;if (2 == arguments.length) var c = arguments[1];else {
+    for (; a < e && !(a in b);) {
+      a++;
+    }if (a >= e) throw new TypeError("Reduce has an empty array without an initial value");c = b[a++];
+  }for (; a < e; a++) {
+    a in b && (c = d(c, b[a], a, b));
+  }return c;
+};
 
 export default connect;
