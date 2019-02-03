dfeeaf00be94e7c8b5397792c9ac0c14d7a4838b
Iago Bruno <httpiago@gmail.com>
2019-02-03 20:58:01 -0300

    fix: Allow containers to be received in an object or in an array

--- src/connect.js
+++ src/connect.js
@@ -1,29 +1,40 @@
 import React from "react";
 import { Subscribe } from "unstated";
 
-export default function function HOC(_containers) {
-  return (WrappedComponent) => {
-    const NewComponent = (props) => (
-      <Subscribe to={_containers}>
-        {(...containers) => {
-          // Transform the array into an object (the name of the classes will be the keys)
-          let mapContainers = containers.reduce((acc, cur) => (acc[cur.constructor.name] = cur, acc), {})
+export default function connect(__containers) {
+  return WrappedComponent => {
+    // Check if __containers is an object
+    let __containersIsAnObject = __containers.constructor === Object;
+    if (__containersIsAnObject) {
+      // Convert object into an array for unstated understand
+      __containers = Object.values(__containers)
+    }
 
-          return <WrappedComponent {...props} containers={mapContainers} />
+    const ConnectedComponent = props => (
+      <Subscribe to={__containers}>
+        {(...containers) => {
+          if (__containersIsAnObject) {
+            // Transform the array into an object (the name of the classes will be the keys)
+            containers = containers.reduce((acc, cur) => ((acc[cur.constructor.name] = cur), acc), {});
+          }
+
+          return <WrappedComponent {...props} containers={containers} />
         }}
       </Subscribe>
     );
-    
+
     // If it is a NextJS page component, run getInitialProps function
-    if (typeof WrappedComponent.getInitialProps !== 'undefined') {
-      NewComponent.getInitialProps = async (args) => {
+    if (typeof WrappedComponent.getInitialProps !== "undefined") {
+      ConnectedComponent.getInitialProps = async args => {
         return await WrappedComponent.getInitialProps(args);
       }
     }
-    
-    return NewComponent
-  }
+
+    return ConnectedComponent;
+  };
 }
 
 // [].reduce polyfill
 if (!Array.prototype.reduce) Array.prototype.reduce=function(d){if(null==this)throw new TypeError("Array.prototype.reduce called is null (null) or undefined (undefined)");if("function"!==typeof d)throw new TypeError(d+" is not a function");var b=Object(this),e=b.length>>>0,a=0;if(2==arguments.length)var c=arguments[1];else{for(;a<e&&!(a in b);)a++;if(a>=e)throw new TypeError("Reduce has an empty array without an initial value");c=b[a++]}for(;a<e;a++)a in b&&(c=d(c,b[a],a,b));return c};
+// Object.values({}) polyfill
+if (!Object.values) Object.values=(a)=>{var c=[],b;for(b in a)a.hasOwnProperty(b)&&c.push(a[b]);return c};
